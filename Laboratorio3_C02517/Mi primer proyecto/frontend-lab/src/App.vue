7a11010a325f30452ae03f9fde32ead00acde9cd
Javier <ibrahimcruz06@gmail.com>
2026-03-24 18:44:22 -0600

    2da parte de Laboratorio 3 completada, pediente code challenge

--- Laboratorio3_C02517/Mi primer proyecto/frontend-lab/src/App.vue
+++ Laboratorio3_C02517/Mi primer proyecto/frontend-lab/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Javier Cruz Ruiz"/>
+  <CountriesList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CountriesList,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
