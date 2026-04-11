960caa81cbc930f740f8067c4b71f94263936a77
Javier <ibrahimcruz06@gmail.com>
2026-04-10 18:05:46 -0600

    Laboratorio #5: Se agrega axios a Country Form

--- Laboratorio5_C02517/Mi primer proyecto/frontend-lab/src/components/CountryForm.vue
+++ Laboratorio5_C02517/Mi primer proyecto/frontend-lab/src/components/CountryForm.vue
@@ -51,6 +51,7 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
     data () {
         return {
@@ -60,6 +61,19 @@ export default {
     methods: {
         saveCountry() {
             console.log("Datos a guardar:", this.formData);
+            axios
+                .post("https://localhost:7053/api/Country", {
+                    Name: this.formData.Name,
+                    Continent: this.formData.Continent,
+                    Language: this.formData.Language,
+                })
+                .then(function (response) {
+                    console.log(response);
+                    window.location.href = "/";
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         },
     },
 };
